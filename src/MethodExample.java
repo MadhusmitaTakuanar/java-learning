81260e63e4471afcff1b1577a2e805c51934d8fd
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-06 16:43:32 +0530

    Added java method practice

---
+++ src/MethodExample.java
@@ -0,0 +1,15 @@
+public class MethodExample {
+    public static void greet() {
+        System.out.println("Hello Madhusmita, welcome to Java learning.");
+    }
+    public static void showCity() {
+        System.out.println("My city is Kolkata");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        greet();
+        greet();
+        showCity();
+    }
+}
