b50d0f33e079a9f2f0de389dea37402f833abf78
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-06 16:32:35 +0530

    Added If-Else practice programs

---
+++ src/AgeEligibility.java
@@ -0,0 +1,19 @@
+public class AgeEligibility {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if(age >= 18){
+            System.out.println("You are eligible to vote.");
+        }else{
+            System.out.println("You are not eligible to vote.");
+        }
+
+        int salary = 40000;
+
+        if(salary >= 50000){
+            System.out.println("High salary");
+        }else{
+            System.out.println("Average salary");
+        }
+    }
+}
