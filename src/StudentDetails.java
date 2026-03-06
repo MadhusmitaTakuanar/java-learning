980c216eabdfda1d736e00e44dbbd67c852a090a
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-06 16:15:33 +0530

    Added Operators practice

---
+++ src/StudentDetails.java
@@ -0,0 +1,15 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Madhusmita";
+        int age = 26;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean isGraduate = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Graduate: " + isGraduate);
+    }
+}
