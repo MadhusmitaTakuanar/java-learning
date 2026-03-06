ad0e0e6e8a1c55cf94dbaf76e32616e6d5e2fe9b
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-06 14:58:28 +0530

    First Java practice program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 26;
+        double salary = 50000.50;
+        String name = "Madhusmita";
+        String city = "Kolkata";
+        String experience = "Four Years of Experience";
+
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My salary is " + salary);
+        System.out.println("My City is " + city);
+        System.out.println("My Experience is " + experience);
+
+
+        int experiences = 4;
+        if (experiences >= 3) {
+            System.out.println("Senior Developer");
+        } else {
+            System.out.println("Junior Developer");
+        }
+
+    }
+}
