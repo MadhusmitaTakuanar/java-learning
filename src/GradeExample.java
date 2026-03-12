a43cd1af59b843824daab1645a391f3ae1a4538d
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-12 16:40:13 +0530

    loop

---
+++ src/GradeExample.java
@@ -0,0 +1,18 @@
+public class GradeExample {
+    public static void main(String[] args) {
+        int marks = 55;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        }
+        else if (marks >= 70) {
+            System.out.println("Grade B");
+        }
+        else if (marks >= 50) {
+            System.out.println("Grade C");
+        }
+        else {
+            System.out.println("Fail");
+        }
+    }
+}
