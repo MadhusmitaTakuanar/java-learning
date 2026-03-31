d746f20681b8bc3ec6f058283e5bb587a6291756
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-31 16:39:11 +0530

    Day 18 - Constructor in student class

--- src/student.java
+++ src/student.java
@@ -3,6 +3,13 @@ public class student {
     int age;
     double marks;
 
+    // Constructor
+    public student(String name, int age, double marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
     public void showDetails() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
@@ -10,20 +17,10 @@ public class student {
     }
 
     public static void main(String[] args) {
-        student s1 = new student();
-        s1.name = "Madhusmita";
-        s1.age = 26;
-        s1.marks = 85.5;
 
-        student s2 = new student();
-        s2.name = "Keshab";
-        s2.age = 28;
-        s2.marks = 78.0;
-
-        student s3 = new student();
-        s3.name = "Shivantika";
-        s3.age = 26;
-        s3.marks = 100;
+        student s1 = new student("Madhusmita", 26, 85.5);
+        student s2 = new student("Keshab", 28, 78.0);
+        student s3 = new student("Shivantika", 26, 100);
 
         s1.showDetails();
         s2.showDetails();
