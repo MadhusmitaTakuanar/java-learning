4afbb4f88cbe84f61f89b3a76c4cd51dd123d77e
madhusmita-pradhishila <madhusmita.kuanar@pradhishila.com>
2026-03-31 16:25:03 +0530

    Day 17 - Methods inside class and multiple objects

--- src/student.java
+++ src/student.java
@@ -3,13 +3,30 @@ public class student {
     int age;
     double marks;
 
+    public void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+
     public static void main(String[] args) {
         student s1 = new student();
         s1.name = "Madhusmita";
         s1.age = 26;
         s1.marks = 85.5;
-        System.out.println("Name:" + s1.name);
-        System.out.println("Age:" + s1.age);
-        System.out.println("Marks:" + s1.marks);
+
+        student s2 = new student();
+        s2.name = "Keshab";
+        s2.age = 28;
+        s2.marks = 78.0;
+
+        student s3 = new student();
+        s3.name = "Shivantika";
+        s3.age = 26;
+        s3.marks = 100;
+
+        s1.showDetails();
+        s2.showDetails();
+        s3.showDetails();
     }
 }
